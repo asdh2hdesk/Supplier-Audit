62ca604df3a348568f45d54632ab8b19c8ac27f8
Rakesh Nulekar <141513551+rakeshnulekar@users.noreply.github.com>
2025-07-10 12:04:38 +0530

    1. serial no added

--- supplier_audit/models/audit_checklist.py
+++ supplier_audit/models/audit_checklist.py
@@ -45,7 +45,8 @@ class AuditChecklistQuestion(models.Model):
     _description = 'Audit Checklist Question'
     _order = 'sequence, id'
 
-    sequence = fields.Integer('Sl. No', default=10, help="Serial number of the question")
+    sequence = fields.Integer('Sequence Number', default=10, help="Serial number of the question")
+    sl_no = fields.Integer('Sl.No', compute= '_compute_serial_no')
     name = fields.Text('Question', required=True)
     evidence = fields.Text('Evidence', help="Details of evidence required to evaluate the question")
     scoring_criteria = fields.Text('Scoring Criteria', help="Criteria for scoring the question")
@@ -80,6 +81,15 @@ class AuditChecklistQuestion(models.Model):
                 default_category = self.env['audit.question.category'].create({'name': 'General'})
             vals['category_id'] = default_category.id
         return super(AuditChecklistQuestion, self).create(vals)
+
+    @api.depends('sequence', 'checklist_id')
+    def _compute_serial_no(self):
+        for line in self.mapped('checklist_id'):
+            sl_no = 1
+            for lines in line.question_ids:
+                lines.sl_no = sl_no
+                sl_no += 1
+
 
 
 class AuditQuestionCategory(models.Model):
